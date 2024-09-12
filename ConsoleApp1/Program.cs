5f7cf741c2b6d3abf7610f7911dd5eaf6edc50c2
johe07003 <johe07003@E-PW05C37X>
2024-09-12 12:27:48 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Hej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Du är nästan klar med kapitel 1.//
+            Console.WriteLine("Din dators bios har exploderat");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\.");
+            //Du har lärt dig skriva ut tecken som &quot; och \.//
+        }
+    }
+}
